4d26e518fd28fd296bd42b7ecc4679846ed125e7
Rob <greenr27@uni.coventry.ac.uk>
2018-02-12 17:40:23 +0000

    continued battle system, added hit chance and pseudocode for dmg calculation

--- battle.cpp
+++ battle.cpp
@@ -1,44 +1,67 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 
-int battle() //battle function will eventually take enemy ID as argument
+void battle() //battle function will eventually take enemy ID as argument
 {
     cout << "Select a battle option" << endl;
 	cout << "1 - Basic Attack" << endl;
 	cout << "2 - Ability" << endl;
 	cout << "3 - Magic" << endl;
 	cout << "4 - Item" << endl; //allows the player to select an option
+	while(true){
+    int hit;
+    int hitChance = 90;
+    int mageHitChance = 90;
 	short choice;
 	cin >> choice;
-	switch (choice) {
+	switch (choice){
 	case 1:
+	    {hit = rand() % 100+1;
+	    if(hitChance >= hit){
+        //pseudo code: mobHP = mobHP - wpnatk - 1.4*attack
+		//if PC == RANGED: mobHP = mobHP - wpnatk + 1.2*dexterity
 		cout << "basic attack" << endl;
-		break;
-//Basic attack
+        }
+		else{
+        cout << "You missed!" << endl;
+		}
+		break;}
 	case 2:
 		cout << "Ability" << endl;
+		//pseudo code: mobHP = mobHP - abilitydmg - 1.3*level - 1.4*main stat
 		break;
-//Ability
 	case 3:
-		cout << "Magic" << endl;
+        hit = rand() % 100+1;
+	    if(mageHitChance >= hit){
+        //pseudo code: mobHP = mobHP - magicdmg - 1.6*intelligence
+		cout << "Magic attack" << endl;
+        }
+		else{
+        cout << "You missed!" << endl;
+		}
 		break;
-//Magic
 	case 4:
-		cout << "Item" << endl;
+	    /*
+	    string itemChoice;
+	    SELECT itemName FROM inventory WHERE usable = 1;
+	    while(cur->step())
+            cout << cur->get_text(0) << endl;
+        cout << "Choose an item" << endl;
+        cin >> itemChoice;
 		break;
-//Item
+		*/
+    default:
+        cout << "Invalid choice!" << endl;
+        continue;
+        }
+
 	}
-	short i;
-	cin >> i;
-	
-	return 0;
+
 }
+
 
 int main()
 {
