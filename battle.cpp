eeecc69badafb4d7ce1a7a83d32000fcc85574fd
Rob <greenr27@uni.coventry.ac.uk>
2018-02-26 17:47:23 +0000

    included player, enemy and weapon stats in damage calculations, made magic mage-only

--- battle.cpp
+++ battle.cpp
@@ -1,59 +1,89 @@
 #include <iostream>
 #include <cstdlib>
 #include "passive-output.cpp"
+#include "player.h"
+#include "Mob.h"
+#include "Item.h"
 
 using namespace std;
 
 
-void battle() //battle function will eventually take enemy ID as argument
-{ //int turn = 0;
+void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, mob and item objects as parameters
+											   //item object should be players equipped weapon
+{	
+	//int turn = 0;
     //passive();
+	cout << "Level " << Enemy.getLevel() << " " << Enemy.getName() << " appears!" << endl;
     cout << "Select a battle option" << endl;
 	cout << "1 - Basic Attack" << endl;
 	cout << "2 - Ability" << endl;
 	cout << "3 - Magic" << endl;
 	cout << "4 - Item" << endl; //allows the player to select an option
-	while(true){
-    int hit;
-    int hitChance = 90;
-    int mageHitChance = 90;
+	while(turn % 2== 0){ //While it is the players turn:
+    int hitChance = Weapon.getHitChance();
+	int mageHitChance = 50 + PC.intelligence*5
+	int incomingDamage;
 	short choice;
 	cin >> choice;
 	switch (choice){
 	case 1:
-	{	turn = turn + 1;
+	{	
+		turn = turn + 1;
 		hit = rand() % 100+1;
-	    if(hitChance >= hit){
-        //pseudo code: mobHP = mobHP - wpnatk - 1.4*attack
-		//if PC == RANGED: mobHP = mobHP - wpnatk + 1.2*dexterity
-		cout << "basic attack" << endl;
+	    if(hitChance >= hit) //If player rolls within his hit chance...
+		{
+			if (Weapon.getRange > 0) //If the player is using a ranged weapon...
+			{
+				incomingDamage = Weapon.getDmg() - 1.2*PC.dexterity + Enemy.getArmor(); //Calculates incoming damage, using player, enemy and weapon attributes
+				Enemy.setHP(Enemy.getHP() - incomingDamage);
+			}
+			else //If the player is using a melee weapon...
+			{
+				incomingDamage = Weapon.getDmg() - 1.2*PC.attack + Enemy.getArmor();
+				Enemy.setHP(Enemy.getHP() - incomingDamage);
+			}
+			cout << "You hit the enemy for "+incomingDamage << endl;
         }
-		else{
-        cout << "You missed!" << endl;
+		else
+		{ //If the player misses...
+			cout << "You missed!" << endl;
 		}
-		break;}
+		break;
+	}
 	case 2:
-	{	turn = turn + 1;
+	{	
+		turn = turn + 1;
 		cout << "Ability" << endl;
-		//pseudo code: mobHP = mobHP - abilitydmg - 1.3*level - 1.4*main stat
+		//ability-use();
+		//incomingDamage = abilityDamage;
+		//Enemy.setHP(Enemy.getHP() - abilityDamage);
 		break;
 	}
 	case 3:
 	{
-	turn = turn + 1;
-        hit = rand() % 100+1;
-	    if(mageHitChance >= hit){
-        //pseudo code: mobHP = mobHP - magicdmg - 1.6*intelligence
-		cout << "Magic attack" << endl;
+		if(PC.characterClass == "Mage") //MAGIC WILL BE MAGE ONLY FOR SIMPLICITIES SAKE
+		{
+			turn = turn + 1;
+			hit = rand() % 100 + 1;
+			if(mageHitChance >= hit)
+			{
+				//pseudo code: mobHP = mobHP - magicdmg - 1.6*intelligence
+				cout << "Magic attack" << endl;
+			}
+			else 
+			{
+				cout << "You missed!" << endl;
+			}
         }
-		else{
-        cout << "You missed!" << endl;
+		else
+		{
+			cout << "Your feeble mind cannot comprehend magic!" << endl;
 		}
 		break;
 	}
 	case 4:
 	{
-	turn = turn + 1;
+		turn = turn + 1;
 	    /*
 	    string itemChoice;
 	    SELECT itemName FROM inventory WHERE usable = 1;
@@ -70,6 +100,9 @@ void battle() //battle function will eventually take enemy ID as argument
         }
 
 	}
+	
+	
+	/* ENEMY BATTLE PHASE GOES HERE*/
 
 }
 
