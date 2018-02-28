d8133fe76007c794e908ef82691eeed5bcb1e7a5
Robert Green <greenr27@uni.coventry.ac.uk>
2018-02-28 15:24:55 +0000

    Added sql functionality, player data now stored in instance of PlayerCharacter struct as local variables for use in battle function

--- battle.cpp
+++ battle.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
 #include <cstdlib>
-#include "passive-output.cpp"
-#include "player.h"
-#include "Mob.h"
-#include "Item.h"
+#include <string>
+//#include "passive-output.cpp"
+//#include "Player.h"
+//#include "Mob.h"
+//#include "Item.h"
+#include "libsqlite.hpp"
 
 using namespace std;
 
+struct PlayerCharacter //Pulls character data from database and saves it as local variables
+{
+  int playerClass;
+  int level;
+  int exp;
+  int health;
+  int remHealth;
+  int attack;
+  int defence;
+  int intelligence;
+  int perception;
+  int dexterity;
+  
+  PlayerCharacter()
+  {
+  short id = Player::CharacterID;
+  sqlite::sqlite db("RPGDatabase.db");
+  auto cur = db.get_statement();
+  cur->set_sql("SELECT * FROM CharacterData WHERE CharacterID = ?");
+  cur->prepare();
+  cur->bind(1, id);
+  cur->step();
+  string name = cur->get_text(1);
+  playerClass = cur->get_int(2);
+  level = cur->get_int(3);
+  exp = cur->get_int(4);
+  health = cur->get_int(5);
+  remHealth = cur->get_int(6);
+  attack = cur->get_int(7);
+  defence = cur->get_int(8);
+  intelligence = cur->get_int(9);
+  perception = cur->get_int(10);
+  dexterity = cur->get_int(11);
+  
+  //cout << name << " " << playerClass << " " << level << " " << exp << " " << health << " " << remHealth << " " << attack << " " << defence << " " << intelligence << " " << perception << " " << dexterity << endl;
+  }
+};
 
-void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, mob and item objects as parameters
-											   //item object should be players equipped weapon
+void battle(PlayerCharacter PC, Monster Mob, Item Weapon) 
+  //battle function takes player, mob and item objects as parameters
+  //item object should be players equipped weapon
 {	
-	//int turn = 0;
+	  //int turn = 0;
     //passive();
 	cout << "Level " << Enemy.getLevel() << " " << Enemy.getName() << " appears!" << endl;
     cout << "Select a battle option" << endl;
@@ -20,7 +60,7 @@ void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, m
 	cout << "3 - Magic" << endl;
 	cout << "4 - Item" << endl; //allows the player to select an option
 	while(turn % 2== 0){ //While it is the players turn:
-    int hitChance = Weapon.getHitChance();
+  int hitChance = Weapon.getHitChance();
 	int mageHitChance = 50 + PC.intelligence*5
 	int incomingDamage;
 	short choice;
@@ -29,7 +69,7 @@ void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, m
 	case 1:
 	{	
 		turn = turn + 1;
-		hit = rand() % 100+1;
+		int hit = rand() % 100+1;
 	    if(hitChance >= hit) //If player rolls within his hit chance...
 		{
 			if (Weapon.getRange > 0) //If the player is using a ranged weapon...
@@ -61,7 +101,7 @@ void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, m
 	}
 	case 3:
 	{
-		if(PC.characterClass == "Mage") //MAGIC WILL BE MAGE ONLY FOR SIMPLICITIES SAKE
+		if(PC.characterClass == 4) //MAGIC WILL BE MAGE ONLY FOR SIMPLICITIES SAKE
 		{
 			turn = turn + 1;
 			hit = rand() % 100 + 1;
@@ -92,7 +132,7 @@ void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, m
         cout << "Choose an item" << endl;
         cin >> itemChoice;
 		break;
-		*/
+		
 	}
     default:
         cout << "Invalid choice!" << endl;
@@ -106,8 +146,7 @@ void battle(Player PC, Mob Enemy, Item Weapon) //battle function takes player, m
 
 }
 
-
 int main()
 {
-	battle();
+  //PlayerCharacter test;
 }
