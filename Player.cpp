e1340edec659dcd48d0c7f35e2d3102ab67c9388
Oana Donose (donoseo) <donoseo@coventry.ac.uk>
2018-03-15 14:49:30 +0000

    added exp gain, lvl up and stats raise functions in .cpp

--- Player.cpp
+++ Player.cpp
@@ -58,7 +58,171 @@ void Player::setCharacterClass(int classChoice) // Sets character class based up
             throw invalid_argument( "Received invalid class choice" );
     }
 }
+void Player::gainExperience(int experienceReward) //input is the experience rewarded from quests or battles
+{
+    cout<<"Good job! You've earned "<<experienceReward<<" experience points."<<endl;//message for the player
+    
+    string databaseFile = "RPGDatabase.db";
 
+        try
+        {
+            //getting the experience from database
+            sqlite::sqlite db( databaseFile );
+            auto cur = db.get_statement();
+            cur->set_sql( "SELECT CharacterExperience FROM CharacterData WHERE CharacterID = ?");
+            cur->bind(1,characterID)
+            cur->prepare();
+            cur->step();
+            
+            experience = cur->get_int(0); //the experience the player already has
+            
+            //calculating new experience
+            experience += experienceReward;
+            
+            //changing experience from database
+            cur->set_sql("UPDATE CharacterData SET CharacterExperience = ? WHERE CharacterID = ?");
+            cur->bind(1,experience);
+            cur->bind(2,characterID);
+            cur->prepare();
+            cur->step();
+        }
+        catch( sqlite::exception e )      // catch all sql issues
+        {
+            cerr << e.what() << endl;
+        }
+}
+
+void Player::levelUp() //since the story is linear this can only do so much....
+{
+    
+    cout<<"Congratulations! You leveled up!"<<endl;
+    
+    string databaseFile = "RPGDatabase.db";
+
+        try
+        {
+            //getting the level from database
+            sqlite::sqlite db( databaseFile );
+            auto cur = db.get_statement();
+            cur->set_sql( "SELECT CharacterLevel FROM CharacterData WHERE CharacterID = ?");
+            cur->bind(1,characterID)
+            cur->prepare();
+            cur->step();
+            level = cur->get_int(0);
+            
+            //calculating new level            
+            level += 1;
+     
+            cout<<"You are now level "<<level<<"!"<<endl;
+            
+            //changing level from database
+            cur->set_sql("UPDATE CharacterData SET CharacterLevel = ? WHERE CharacterID = ?;");
+            cur->bind(1,level);
+            cur->bind(2,characterID);
+            cur->prepare();
+            cur->step();
+        }
+        catch( sqlite::exception e )      // catch all sql issues
+        {
+            cerr << e.what() << endl;
+        }
+}
+
+void Player::raiseStats() //call when player levels up
+{
+    string databaseFile = "RPGDatabase.db";
+
+        try
+        {
+            //getting the level from database
+            sqlite::sqlite db( databaseFile );
+            auto cur = db.get_statement();
+            cur->set_sql( "SELECT * FROM CharacterData WHERE CharacterID = ?");
+            cur->bind(1,characterID)
+            cur->prepare();
+            cur->step();
+            name = cur->get_text(1);
+            classID = cur->get_int(2);
+            level = cur->get_int(3);
+            health = cur->get_int(5);
+            attack = cur->get_int(7);
+            defence = cur->get_int(8);
+            intelligence = cur->get_int(9);
+            perception = cur->get_int(10);
+            dexterity = cur->get_int(11);
+            
+            //calculating new stats
+            switch(classID)
+            {
+                case 1: //warrior
+                    {
+                    health = health + 2;
+                    attack = attack + 3;
+                    defence = defence + 4;
+                    intelligence = intelligence + 1;
+                    perception = perception + 1;
+                    dexterity = dexterity + 1;
+                    break;
+                    }
+                case 2: //rogue
+                    {
+                         health = health + 1;
+                         attack = attack + 3;
+                         defence = defence + 2;
+                         intelligence = intelligence + 1;
+                         perception = perception + 1;
+                         dexterity = dexterity + 4;
+                         break;
+                    }
+                case 3: //archer
+                    {
+                         health = health + 1;
+                         attack = attack + 3;
+                         defence = defence + 1;
+                         intelligence = intelligence + 1;
+                         perception = perception + 4;
+                         dexterity = dexterity + 2;
+                         break;
+                    }
+                case 4: //mage
+                    {
+                        health = health + 1;
+                        attack = attack + 1;
+                        defence = defence + 1;
+                        intelligence = intelligence + 4;
+                        perception = perception + 3;
+                        dexterity = dexterity + 2;
+                        break;
+                    }
+                 default:
+                        throw invalid_argument( "Received invalid class choice" );
+            }
+            
+            //notifying the player
+            cout<<"Updated stats!"<<endl;
+            cout<<"Health: "<<health<<endl;
+            cout<<"Attack: "<<attack<<endl;
+            cout<<"Defence: "<<defence<<endl;
+            cout<<"Intelligence: "<<intelligence<<endl;
+            cout<<"Perception: "<<perception<<endl;
+            cout<<"Dexterity: "<<dexterity<<endl;
+            
+            //changing stats in the database
+            cur->set_sql("UPDATE CharacterData SET CharacterHealth = ?,CharacterAttack = ?, CharacterDefence = ?, CharacterIntelligence = ?, CharacterPerception =?, CharacterDexterity =? WHERE CharacterID = ?;");
+            cur->bind(1,health);
+            cur->bind(2,attack);
+            cur->bind(3,defence);
+            cur->bind(4,intelligence);
+            cur->bind(5,perception);
+            cur->bind(6,dexterity);            
+            cur->prepare();
+            cur->step();
+        }
+        catch( sqlite::exception e )      // catch all sql issues
+        {
+            cerr << e.what() << endl;
+        }
+}
 /*
 // Tests for Player class code
 Player mainCharacter;
