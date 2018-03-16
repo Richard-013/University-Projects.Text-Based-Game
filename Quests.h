d0204ef1f6140d460989e37cf2982979668801e5
Andreea Ghinies <ghiniesa@uni.coventry.ac.uk>
2018-03-16 11:13:57 +0000

    modified

--- Quests.h
+++ Quests.h
@@ -3,17 +3,22 @@
 #include <string>
 
 using namespace std;
-
 class Quests {
+  
   string qname;
   string qreward;
   int qpoints; 
-  int qnumber;
-   
- public:
+  int qnumber; 
+  
+  public:
   Quests();
-   void insertDetails (string qname, int qnumber, string qreward, int qpoints);
-   void SeeDetails(int qnumber);
+  
+  
+   void insertDetails (string qname, string qreward, int qpoints);
+  string getReward(int qnumber);
+  
+  
+  void SeeDetails(int qnumber);
 };
 
 
