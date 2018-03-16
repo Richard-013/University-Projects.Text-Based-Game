991c6b48b441d21db60a22f49c8857f767068827
Andreea Ghinies <ghiniesa@uni.coventry.ac.uk>
2018-03-16 11:14:38 +0000

    changed functions

--- Quests.cpp
+++ Quests.cpp
@@ -3,7 +3,7 @@
 #include "Quests.h"
 #include "libsqlite.hpp"
 
-using namespace std;
+//using namespace std;
 
 
 Quests::Quests()
@@ -11,24 +11,23 @@ Quests::Quests()
   //ctor
 };
 
-void Quests::insertDetails(string qname, int qnumber,string qreward,int qpoints)
-{ cout <<qname <<"," <<qnumber<< endl;
+void Quests::insertDetails(string qname, string qreward, int qpoints )
+{ //cout <<qname <<"," <<qpoints<< endl;
   string databaseFile = "RPGDatabase.db";
  try 
  {
   
-sqlite::sqlite db( databaseFile );
+        sqlite::sqlite db(databaseFile);
         auto cur = db.get_statement();
-		
-        cur->set_sql( "INSERT INTO QuestsData (QuestName,QuestNumber,QuestReward,QuestPoints) "
-                      "VALUES ( ? , ? , ?, ?  );" );
-  
+        cur->set_sql( "INSERT INTO QuestData (QuestName,QuestReward,QuestPoints) VALUES(?,?,?)");
+        //cur->set_sql ("SELECT * FROM NPCData;");      
         cur->prepare();
-        cur->bind(1,qname );
-        cur->bind(2,qnumber);
+        cur->bind(1,qname);
+        //cur->bind(2,qnumber); It's a primary key
         cur->bind(3,qreward);
         cur->bind(4,qpoints);
         cur->step();
+       
  }
 
 catch(sqlite::exception e ) //catch all sql issues
@@ -36,25 +35,31 @@ catch(sqlite::exception e ) //catch all sql issues
   cerr << e.what() << endl;
  
 }
+ //return qreward;
 };
 
-void Quests::SeeDetails(int qnumber)  // Allows the player to load their progress from the information stored in the database
+string Quests::getReward(int qnumber)  // Allows the player to load their progress from the information stored in the database
 {
-	string databaseFile = "RPGDatabase.db";
+	  string databaseFile = "RPGDatabase.db";
 
     try
     {
         sqlite::sqlite db( databaseFile );
         auto cur = db.get_statement();
 		
-        cur->set_sql( "SELECT * FROM QuestsData WHERE QuestsNumber = ?");
+        cur->set_sql( "SELECT * FROM QuestData WHERE QuestsNumber = ?");
 		cur->prepare();
-		cur->bind( 4, qnumber );
-		cur->step();
+		cur->bind(1,qnumber);
+    cur->step();
+    qreward=cur->get_text(2);
 		
     }
     catch( sqlite::exception e )      // catch all sql issues
     {
         cerr << e.what() << endl;
     }
+   return qreward;
 }
+
+//void Quests::setIsDone(string qname, int )
+
